98e7aa302c6917a35209ad1bb7847b3c7bd7265f
Andros Slowley <androsslowley@andross-mbp.home>
2016-12-04 11:45:19 -0500

    Initial Commit

---
+++ HistoricalEvents2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HistoricalEvents2
+//
+//  Created by Andros Slowley on 12/4/16.
+//  Copyright © 2016 Andros Slowley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
